81692185f640d4eab743f935e1304096dc248cf4
chimalongy <me.chimaobi@gmail.com>
2026-06-22 06:25:12 +0100

    body limit 15mb

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '15mb',
+    },
+  },
 };
 
 export default nextConfig;
